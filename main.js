c864f4685620855bda43dd48f227d8bc8eb0630b
Borjamrd <borjamrd1@gmail.com>
2021-11-10 18:55:08 +0100

    ways to select and change values

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
 
+
+/*---Turno al jugador X---*/
+
+
+function prueba(){
+    let message = document.querySelector(".section-game")
+    message.innerHTML += '<p class="changeTurn">Le toca al siguiente jugador</p>';
+     
+}
+cell = document.querySelectorAll("#game .spaceX");
+Array.from(cell).forEach(function(button){
+    button.addEventListener("click",function(e){
+        button.innerHTML = "X";
+   })
+   document.getElementsByTagName("button").setAttributte("class", "spaceY") 
+})
+
+
+/*---Turno al jugador Y ---*/
+
+cell = document.querySelectorAll("button");
+
+Array.from(cell).forEach(function(button){
+    button.addEventListener("click",function(e){
+        button.innerHTML = "Y"
+    })
+})
+
+
+/*Mensaje de cambio de turno*/
+
+let message = document.querySelector(".section-game")
+message.innerHTML += '<p class="changeTurn">Le toca al siguiente jugador</p>';
