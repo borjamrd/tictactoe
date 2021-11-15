364258f66a11a8e7f0f7b13e7785d942259aa923
Borjamrd <borjamrd1@gmail.com>
2021-11-15 21:55:42 +0100

    start again again

--- main.js
+++ main.js
@@ -1,20 +1,56 @@
 
+const winComb = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
 
 let currentTurn = 1;
+let gameActive = true;
+const finishGame = false;
+const playerX = "X";
+const playerY = "Y";
+const cells = document.querySelectorAll("[cell]");
+const newGameButton = document.getElementById('newGame')
+const board = document.getElementById('board')
 
-let board = [
+
+
+newGameButton.addEventListener("click", newGame);
+
+function newGame (){
+    gameActive = false;
+    cells.forEach(cell => {
+        cell.classList.remove(playerX);
+        cell.classList.remove(playerY);
+        cell.removeEventListener(click, handleClick)
+        cell.addEventListener("click", handleClick) //---introducir once --//)
+    })
+}
+
+function handleClick(cell){
+    const space = cell.target;
+
+}
+
+
+
+/* let board = [
     [undefined,undefined,undefined],
     [undefined,undefined,undefined],
     [undefined,undefined,undefined]
-]
+] */
 
-function move(row, column){
-    console.log(board[row,column])
-
-}
 
 
 /*Mensaje de cambio de turno*/
 
 let message = document.querySelector(".section-game")
 message.innerHTML += '<p class="changeTurn">Le toca al siguiente jugador</p>';
+
