6a7aaafcee148c63fc9fe5e77c36a47e3b4f2364
Borjamrd <borjamrd1@gmail.com>
2021-11-15 23:26:15 +0100

    very lost

--- main.js
+++ main.js
@@ -16,13 +16,21 @@ let gameActive = true;
 const finishGame = false;
 const playerX = "X";
 const playerY = "Y";
-const cells = document.querySelectorAll("[cell]");
-const newGameButton = document.getElementById('newGame')
-const board = document.getElementById('board')
-
+const allCells = document.querySelectorAll("[cell]");
+const newGameButton = document.getElementById("newGame");
+const board = document.getElementById("board");
+const message = document.getElementById("message")
 
 
 newGameButton.addEventListener("click", newGame);
+
+function checkWin(currentClass) {
+    return winComb.some(combination => {
+      return combination.every(index => {
+        return cells[index].classList.contains(currentClass)
+      })
+    })
+  }
 
 function newGame (){
     gameActive = false;
@@ -32,25 +40,57 @@ function newGame (){
         cell.removeEventListener(click, handleClick)
         cell.addEventListener("click", handleClick) //---introducir once --//)
     })
+    setBoardHoverClass()
+  message.classList.remove('show')
 }
 
-function handleClick(cell){
-    const space = cell.target;
+function handleClick(event){
+    const cell = event.target;
+    const currentClass = playerX ? gameActive: playerY;
+    putMarkonCell(cell, currentClass)
+        if(checkWin(currentClass)){
+            console.log("Juego finalizado"); //*generar texto para mostrar cambio de turno*//
+        }else{
+            changeTurn()
+        }
 
 }
 
+function engGame(draw){
+    if(draw){
+        message.innerHTML = "Draw"
+    } else {
+        message.innerHTML = `${gameActive ? "O" : "X"} wins!`
+    }
+    message.classList.add("show")
+}
 
+function isDraw(){
+    return[...allCells].every(cell =>{
+        return cell.classList.contains(playerX) || cell.classList.contains(playerY);
+    })
+}
 
-/* let board = [
-    [undefined,undefined,undefined],
-    [undefined,undefined,undefined],
-    [undefined,undefined,undefined]
-] */
+function  putMarkonCell(cell, currentClass) {
+    cell.classList.add(currentClass)
+  }
+function changeTurn(){
+    gameActive =! gameActive;
+}
 
+function setBoardHoverClass(){
+    board.classList.remove(playerX);
+    board.classList.remove(playerY);
+    if (gameActive) {
+        board.classList.add(playerY)
+      } else {
+        board.classList.add(playerX)
+      }
 
+}
 
-/*Mensaje de cambio de turno*/
-
+/* 
 let message = document.querySelector(".section-game")
 message.innerHTML += '<p class="changeTurn">Le toca al siguiente jugador</p>';
 
+ */
